3191f8a1bf7d762dbb3537e587a29b43b077685e
anoddnumber <suchanoddnumber@gmail.com>
2017-11-26 11:01:14 -0800

    Randomizer

--- src/utilities/randomizer.py
+++ src/utilities/randomizer.py
@@ -1,3 +1,4 @@
+import random
 
 
 def roll_dice(num_dice=1, num_sides=6):
@@ -7,16 +8,36 @@ def roll_dice(num_dice=1, num_sides=6):
     :param num_sides: The number of sides that each die has
     :return: A list of integers representing each die result.
     """
-    pass
+    if num_dice <= 0 or num_sides <= 0:
+        return []
+
+    dice_rolls = []
+    for i in range(num_dice):
+        dice_rolls.append(random.randint(1, num_sides))
+
+    return dice_rolls
 
 
 def randomize_teams(num_players, num_teams=2):
-    pass
+    shuffled_list = random.sample(range(1, num_players + 1), num_players)
+    num_players_per_team = int(num_players / num_teams)
+    num_teams_with_additional_player = num_players % num_teams
+    num_teams_without_additional_player = num_teams - num_teams_with_additional_player
+
+    randomized_teams = []
+    index = 0
+    for i in range(num_teams_with_additional_player):
+        randomized_teams.append(shuffled_list[index:index + num_players_per_team + 1])
+        index += num_players_per_team + 1
+
+    for i in range(num_teams_without_additional_player):
+        randomized_teams.append(shuffled_list[index:index + num_players_per_team])
+        index += num_players_per_team
+
+    return tuple(randomized_teams)
 
 
 def randomize_seating(num_players):
-    pass
-
-
-
-
+    if num_players <= 0:
+        return tuple([])
+    return tuple(random.sample(range(1, num_players + 1), num_players))
