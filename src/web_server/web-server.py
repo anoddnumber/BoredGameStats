09a995873f8a12f95cdf0d86765656e0899ee242
Jonathan Tsai <modopo@gmail.com>
2017-12-03 11:48:52 -0800

    Validation for empty form fields

--- src/web_server/web-server.py
+++ src/web_server/web-server.py
@@ -1,30 +1,43 @@
-from flask import Flask, render_template, request, redirect, url_for
-import random
+from flask import Flask, render_template, request, \
+    redirect, url_for, flash
 import src.utilities.randomizer as randomizer
 
 app = Flask(__name__)
 
+# Homepage/Hub
 @app.route('/')
 @app.route('/boredgames')
 def homepage():
     return render_template('index.html')
 
+# Roll dice function
 @app.route('/boredgames/rolldice', methods = ['GET', 'POST'])
 def dice():
     if request.method == 'POST':
-        dies = int(request.form['dies'])
-        sides = int(request.form['sides'])
-        result = randomizer.roll_dice(dies, sides)
+        dies = request.form['dies']
+        sides = request.form['sides']
+        if not dies or not sides:
+            flash("If dies and sides are left empty, we rolled to 1 dice with 6 sides beches~")
+            result = randomizer.roll_dice()
+        else:
+            result = randomizer.roll_dice(int(dies), int(sides))
         return render_template("roll_dice.html", result=result)
     else:
         return render_template('roll_dice.html')
 
+# Team assign
 @app.route('/boredgames/teams', methods = ['GET', 'POST'])
 def teams():
     if request.method == 'POST':
-        team = int(request.form['team'])
-        players = int(request.form['players'])
-        result = randomizer.randomize_teams(players, team)
+        players = request.form['players']
+        if not players:
+            flash("Fill in the number of players first")
+            return redirect("/boredgames/teams")
+        team = request.form['team']
+        if not team:
+            result = randomizer.randomize_teams(int(players))
+        else:
+            result = randomizer.randomize_teams(int(players), int(team))
         return render_template('teams.html', result = result)
     else:
         return render_template('teams.html')
