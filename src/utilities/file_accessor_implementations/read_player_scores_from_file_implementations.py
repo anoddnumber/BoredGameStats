f0f4aef49152e74252f13f8823acf7579a2198f2
Samson Wong <samswong@amazon.com>
2017-12-14 22:03:43 -0800

    Added implementation for read_player_scores_from_file

--- src/utilities/file_accessor_implementations/read_player_scores_from_file_implementations.py
+++ src/utilities/file_accessor_implementations/read_player_scores_from_file_implementations.py
@@ -1,2 +1,10 @@
-def read_player_scores_from_file_samson():
-    pass
+def read_player_scores_from_file_samson(file_name):
+    player_scores = []
+    with open(file_name) as f:
+        read_data = f.read()
+        lines = read_data.split('\n')
+        for line in lines:
+            if len(line.strip()) > 0:
+                player_scores.append([int(score) for score in line.split(',')])
+    return player_scores
+
