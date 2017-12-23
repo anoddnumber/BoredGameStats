6e1f84a91236737960c75500da1b5b5e27488a95
cchang40 <34631165+cchang40@users.noreply.github.com>
2017-12-23 10:35:18 -0800

    Update game_statistics_helpers.py
    Added all of my helper functions

--- src/utilities/game_statistics_implementations/game_statistics_helpers.py
+++ src/utilities/game_statistics_implementations/game_statistics_helpers.py
@@ -28,3 +28,38 @@ def wins_jason(my_list, player_id):
             games_won.append(0)
         games_list = []
     return games_won
+
+# transposes the matrix from by player to by games
+def game_scores_matrix_colin(player_scores):
+    num_players = len(player_scores)
+    num_games = len(player_scores[1])
+    game_scores = [[] for game in range(num_games)]
+    for x in range(num_games):
+        game_scores[x] = [i[x] for i in player_scores]
+    return game_scores
+
+# creates a list of max score per game
+def max_score_of_a_game_colin(num_games, game_scores):
+    max_scores = [[] for game in range(num_games)]
+    for x in range(num_games):
+        max_scores[x] = max(game_scores[x])
+    return max_scores
+
+# compares each player's game score to the max score per game
+def win_and_losses_by_player_colin(num_players, num_games, max_scores):
+    win_and_losses = [[] for player in range(num_players)]
+    for player in range(num_players):
+        for game in range(num_games):
+            if player_scores[player][game] == max_scores[game]:
+                win_and_losses[player].append(1)
+            else:
+                win_and_losses[player].append(0)
+    return win_and_losses
+
+# calculates the percentage of wins
+def winning_percentage_colin(num_players, num_games, win_and_losses):
+    winning_percentage = []
+    for results in range(num_players):
+        winning_percentage.append(sum(win_and_losses[results])/num_games)
+    return winning_percentage
+
