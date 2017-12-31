1acfc1e159841a03dcc0a7f7b1dfc29e2f23dc7d
csitsao <csitsao@users.noreply.github.com>
2017-12-31 13:52:37 -0800

    New ct_rollDice
    Caroline's version of rollDice

--- src/web_server/view/rollDice.py
+++ src/web_server/view/rollDice.py
@@ -10,22 +10,43 @@ def dice():
         dice = request.form['dice']
         sides = request.form['sides']
 
-        if dice:
-            if type(sides) is not int and sides:
-                flash("Fill in with actual whole integers")
+        try:
+            dice = int(dice)
+            sides = int(sides)
+
+            if int(dice) < 1 or int(sides) < 1: # catch the negatives first
+                flash("Fill in only positive integers")
                 return redirect('boredgames/rolldice')
             else:
-                flash("Defaulting to 6 sides")
-                result = randomizer.roll_dice(int(dice), 6)
-        elif sides:
-            if type(dice) is not int and dice:
-                flash('Fill in with actual whole integers')
+                result = randomizer.roll_dice(int(dice), int(sides))
+                return render_template("roll_dice.html", result=result)
+
+        except ValueError:
+            if dice != "" or sides != "":
+                flash("Fill in only integers")
                 return redirect('boredgames/rolldice')
-            else:
-                flash('Defaulting to 1 dice')
-                result = randomizer.roll_dice(1, int(sides))
-        else:
-            result = randomizer.roll_dice(int(dice), int(sides))
-        return render_template("roll_dice.html", result=result)
+
+            if not dice and not sides:
+                flash("Please at least input something")
+                return redirect('boredgames/rolldice')
+
+            if dice and not sides:
+                if int(dice) < 1:
+                    flash("Fill in only positive integers")
+                    return redirect('boredgames/rolldice')
+                elif sides == "":
+                    flash("Defaulting to 6 sides")
+                    result = randomizer.roll_dice(int(dice), 6)
+                    return render_template("roll_dice.html", result=result)
+
+            elif sides and not dice:
+                if int(sides) < 1:
+                    flash("Fill in only positive integers")
+                    return redirect('boredgames/rolldice')
+                elif dice == "":
+                    flash('Defaulting to 1 dice')
+                    result = randomizer.roll_dice(1, int(sides))
+                    return render_template("roll_dice.html", result=result)
+
     else:
         return render_template('roll_dice.html')
