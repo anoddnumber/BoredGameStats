be78876d99598343a84aab8f20eb5362ff600d0a
cchang40 <34631165+cchang40@users.noreply.github.com>
2017-12-31 16:18:34 -0800

    Update read_player_scores_from_file_implementations.py
    added colin's implementation

--- src/utilities/file_accessor_implementations/read_player_scores_from_file_implementations.py
+++ src/utilities/file_accessor_implementations/read_player_scores_from_file_implementations.py
@@ -8,3 +8,10 @@ def read_player_scores_from_file_samson(file_name):
                 player_scores.append([int(score) for score in line.split(',')])
     return player_scores
 
+def read_player_scores_from_file_colin(file_name):
+    with open(file_name) as f:
+        lines = f.read().splitlines()
+    empty_lists = [[] for x in range(len(lines))]
+    for x in range(len(empty_lists)):
+        empty_lists[x] = [int(x) for x in lines[x].split(',')] # appends each line in txt file as a comma-separated list
+    return empty_lists
