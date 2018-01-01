6823d002229e67babc64e5543ff9ec8608a12540
Jonathan Tsai <modopo@gmail.com>
2017-12-31 17:31:51 -0800

    Secret Key applied 1st attempt

--- src/website/web_server.py
+++ src/website/web_server.py
@@ -6,7 +6,6 @@ sess = Session()
 
 if __name__ == '__main__':
     app.secret_key = config.APP_SECRET_KEY
-    app.config['SESSION_TYPE'] = 'filesystem'
     sess.init_app(app)
     app.debug = config.app_debug
     app.run(host=config.app_run_host, port=config.app_run_port)
